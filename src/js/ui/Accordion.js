af04184b6a6b889fc7b3a4a9013b495f1601dd7d
ejonghwan <sun87-1@daum.net>
2023-06-04 17:19:07 +0900

    아코디언 접근성 완

--- src/js/ui/Accordion.js
+++ src/js/ui/Accordion.js
@@ -1,13 +1,14 @@
 import Ui from './Ui.js'
 
 class Accordion extends Ui {
-    constructor(el, { oneTab }) {
+    constructor(el, { startTab, oneTab }) {
         super();
 
-        this.selector = el;
+        this.selectName = el;
+        this.startTab = startTab;
         this.el = document.querySelector(el);
-        this.head = this.el.querySelectorAll('.item > .acco_head')
-        this.body = this.el.querySelectorAll('.item > .acco_body')
+        this.heads = this.el.querySelectorAll('.item > .acco_head')
+        this.bodys = this.el.querySelectorAll('.item > .acco_body')
 
         this.init();
 
@@ -17,15 +18,29 @@ class Accordion extends Ui {
 
     init() {
         // console.log('Accordion init')
-        for(let i = 0; i < this.head.length; i++) {
-            this.setAttr(this.head[i], 'data-acco_head', i)
+        for(let i = 0; i < this.heads.length; i++) {
+            this.setAttr(this.heads[i], 'data-acco_head', i)
+            
+
+            //접근성
+            this.setAttr(this.heads[i], 'aria-expanded', 'false')
+            this.setAttr(this.heads[i], 'aria-controls', `${this.selectName.replace(/[^\w\s]/g, '')}_accoHead_${i}`)
+            this.setAttr(this.heads[i], 'id', `${this.selectName.replace(/[^\w\s]/g, '')}_accoBody_${i}`)
+
+            this.setAttr(this.bodys[i], 'data-acco_body', i)
+            this.setAttr(this.bodys[i], 'id', `${this.selectName.replace(/[^\w\s]/g, '')}_accoHead_${i}`)
+            this.setAttr(this.bodys[i], 'aria-labelledby', `${this.selectName.replace(/[^\w\s]/g, '')}_accoBody_${i}`)
         }
 
-        for(let i = 0; i < this.body.length; i++) {
-            this.setAttr(this.body[i], 'data-acco_body', i)
+
+        // 로드 시 열려있어야하는 탭
+        if(this.startTab) {
+            console.log(this.startTab)
+            this.setAttr(this.heads[this.startTab], 'aria-expanded', 'false')
+            this.addClass(this.heads[this.startTab], 'on')
+            this.addClass(this.bodys[this.startTab], 'on')
         }
     }
-
 
     action(e) {
         e.stopPropagation()
@@ -33,8 +48,20 @@ class Accordion extends Ui {
         if(!isTarget) return;
 
         let idx = isTarget.dataset.acco_head
-        this.toggleClass(this.head, isTarget, 'on')
-        this.toggleClass(this.body, this.body[idx], 'on')
+    
+
+        if(isTarget.classList.contains('on')) {
+            console.log('assad')
+            this.removeClass(isTarget, 'on')
+            this.removeClass(this.bodys[idx], 'on')
+            this.setAttr(this.bodys[idx], 'aria-expanded', 'false')
+        } else {
+            this.toggleClass(this.heads, isTarget, 'on')
+            this.toggleClass(this.bodys, this.bodys[idx], 'on')
+            
+            // 접근성
+            this.toggleAttr(this.heads, this.heads[idx], 'aria-expanded', 'false', 'true')
+        }
     }
 } 
 
