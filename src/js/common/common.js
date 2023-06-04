fa5262e105a77fec90d7c56f57e692680e4895d0
ejonghwan <sun87-1@daum.net>
2023-06-04 22:54:58 +0900

    팝업 접근성 빼고 완성

--- src/js/common/common.js
+++ src/js/common/common.js
@@ -8,6 +8,7 @@ const common = {
 
     init: function() {
         this.deviceCheck();
+        this.popOpen();
     },
 
     deviceCheck: function() {
@@ -16,7 +17,48 @@ const common = {
         } else {
             this.val.body.classList.add('pc')
         }
-    }
+    },
+
+
+    popOpen: function() {
+        const popupbtn = document.querySelector('.popup_btn')
+        const popupEl = document.querySelector('.popup')
+        const body = document.querySelector('body')
+
+        popupbtn.addEventListener('click', e => {
+           
+           
+
+            // 임시 테스트
+            if(!popupbtn.classList.contains('on')) {
+                popupbtn.classList.add('on')
+                body.classList.add('popop_active')
+
+                if(popupbtn.classList.contains('header_all_menu')) {
+                    body.classList.add('allmenu')
+                }
+                
+            } else {
+                popupbtn.classList.remove('on')
+                body.classList.remove('popop_active')
+
+                if(popupbtn.classList.contains('header_all_menu')) {
+                    body.classList.remove('allmenu')
+                }
+            }
+
+            
+
+            // body
+            if(!popupEl.classList.contains('on')) {
+                popupEl.classList.add('on')
+            } else {
+                popupEl.classList.remove('on')
+            }
+
+
+        })
+    },
 }
 
 
