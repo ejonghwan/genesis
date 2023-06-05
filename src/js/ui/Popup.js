47e469734efe49bf071898aee85e8a09ba0dbd9b
ejonghwan <sun87-1@daum.net>
2023-06-05 16:53:37 +0900

    팝업 접근성 테스트 완료

--- src/js/ui/Popup.js
+++ src/js/ui/Popup.js
@@ -2,11 +2,16 @@ import Ui from '../ui/Ui.js';
 import Accessibility from './Accessibility.js'
 
 class Popup extends Ui {
-    constructor(el, btn) {
+    constructor(el, btn, { accList, focus }) {
         super();
         this.popupEl = document.querySelector(el)
         this.popupbtn = document.querySelector(btn)
         this.body = document.querySelector('body')
+
+        // 접근성 테스트 
+        this.focus = focus;
+        this.accList = accList;
+        this.acc = new Accessibility(this.accList);
 
         // this.init();
         this.popupbtn.addEventListener('click', this.popOpen.bind(this))
@@ -18,25 +23,26 @@ class Popup extends Ui {
 
 
     popOpen() {
-        // 접근성 테스트 
-        const acc = new Accessibility(['header', 'footer', 'main', '#skip_conts']);
-        acc.disable()
-
-        // acc.focus('.popup')
-
-
         // 임시 테스트
         if(!this.popupbtn.classList.contains('on')) {
+            // 열기
             this.popupbtn.classList.add('on')
             this.body.classList.add('popop_active')
+            this.acc.disable();
 
+            setTimeout(() => { //팝업 열리고 포커스 이동
+                this.acc.focus(this.focus)
+            }, 300)
+            
             if(this.popupbtn.classList.contains('header_all_menu')) {
                 this.body.classList.add('allmenu')
             }
             
         } else {
+            // 닫기
             this.popupbtn.classList.remove('on')
             this.body.classList.remove('popop_active')
+            this.acc.enable()
 
             if(this.popupbtn.classList.contains('header_all_menu')) {
                 this.body.classList.remove('allmenu')
