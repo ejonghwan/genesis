32c74d289f7bc856f8374336d24a713b649d17f6
ejonghwan <sun87-1@daum.net>
2023-06-08 09:56:09 +0900

    메인차량 클릭버그 해결

--- src/js/ui/Ani.js
+++ src/js/ui/Ani.js
@@ -3,23 +3,27 @@ import Ui from './Ui.js'
 
 
 class Ani extends Ui {
-    constructor(el, { limit, els }) {
+    constructor(el, { limit, els, duration }) {
         super();
 
         this.el = el;
         this.num = 0;
         this.limit = limit;
-        this.duration  = 300;
+        this.duration = duration;
         this.startTime = performance.now();
-        this.animate();
         this.els = els;
         // this.prevEl = null;
-        console.log(this.els)
+        this.init();
+        this.animate();
+        
 
+    }
+
+    init() {
         for(let i = 0; i < this.els.length; i++) {
             this.els[i].style.zIndex = 0;
         }
-
+        this.el.style.zIndex = 1;
     }
 
 
@@ -32,11 +36,12 @@ class Ani extends Ui {
         let ani = requestAnimationFrame(this.animate.bind(this))
 
         let timelast = time - this.startTime;
+        // console.log(timelast)
 		let progress = timelast / this.duration || 500;
         progress < 0 && (progress = 0);
 		progress > 1 && (progress = 1);
 
-        console.log('t?', progress * window.innerWidth)
+        // console.log('t?', progress * window.innerWidth)
         
 
         this.num++
@@ -44,14 +49,8 @@ class Ani extends Ui {
             cancelAnimationFrame(ani)
             // this.prevEl.style.zIndex = 0;
             // this.prevEl = this.el;
-
-            // console.log('prevEl ?', this.prevEl)
         }
-
         this.el.style.clip = `rect(0px, ${progress * window.innerWidth}px, 610px, 0px)`
-        this.el.style.zIndex = 1
-
-        // console.log(11)
     }
 
     
