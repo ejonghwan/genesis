4c953bd60b22a3c374ae2e2d1b9f376c6e636c85
ejonghwan <sun87-1@daum.net>
2023-06-17 02:20:40 +0900

    kakao map 커스텀 거의 완료

--- src/js/ui/Common.js
+++ src/js/ui/Common.js
@@ -1,3 +1,9 @@
+import Ui from './Ui.js'
+import Tab from './Tab.js'
+import Accordion from './Accordion.js'
+import Popup from './Popup.js'
+
+
 
 class Common {
     constructor() {
@@ -8,6 +14,89 @@ class Common {
 
     init() {
         this.deviceCheck();
+        this.footer();
+        this.globalHeader();
+    }
+
+    footer() {
+         // 푸터 아코디언
+         new Accordion('.footer_sec.all_menu', {
+            startTab: false,
+            oneTab: true,
+        })
+
+    }
+    globalHeader() {
+           
+            // 햄버거 메뉴 
+            new Popup('.popup.all_menu', {
+                openBtn: '.popup_btn',
+                closeBtn: '.popup_btn',
+                accList: ['#container', 'footer', '#skip_conts'],
+                nextFocus: ".pupup_type1_body",
+                prevFocus: ".popup_btn",
+                dimd: '.dimd',
+                toggle: true,
+            })
+
+            // 햄버거 메뉴 안 아코디언
+            new Accordion('.footer_sec.all_menu.pop', {
+                startTab: false,
+                oneTab: true,
+            })
+
+            // 네비메뉴
+            new Popup('.popup.nav_menu', {
+                openBtn: '.header_nav',
+                closeBtn: '.popup.nav_menu .pop_close',
+                accList: ['#container', 'footer', '#skip_conts'],
+                nextFocus: ".pupup_type1_body",
+                prevFocus: ".popup_btn",
+                dimd: '.dimd'
+            })
+
+            // 네비 안 탭
+           new Tab('.tab.type2', {
+                startTab: 0,
+                data: {
+                    url: "./src/js/api/data/car.json", 
+                    mode: 'cors',
+                },
+                current: function(idx) {
+                    // console.log(idx)
+                },
+                callback: function(idx) {
+              
+                }
+            })    
+
+            const nav_swiper = new Swiper(".swiper.header_nav", {
+                autoHeight : true,
+                pagination: {
+                    el: ".swiper-pagination",
+                    clickable: true
+                },
+                navigation: {
+                    nextEl: ".swiper.header_nav .swiper-button-next",
+                    prevEl: ".swiper.header_nav .swiper-button-prev"
+                },
+                on: {
+                    init() {
+                        // console.log('nav swiper init?')
+                    }
+                }
+            });
+
+
+
+            const menus = document.querySelectorAll('.header_nav > li');
+
+            for(let i = 0; i < menus.length; i++) {
+                menus[i].addEventListener('click', e => {
+                    nav_swiper.slideTo(i)
+                })
+            }
+           
     }
 
 
